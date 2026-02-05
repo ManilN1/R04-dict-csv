ad7b5853b5dc53d0106716233ac2186bff7ac3d9
ManilN1 <2172430@cegepmontpetit.ca>
2026-02-05 11:27:05 -0500

    exos 0 a 2 finis

--- Ex1_OS.py
+++ Ex1_OS.py
@@ -3,38 +3,57 @@ os.chdir(os.path.dirname(__file__)) # cette ligne change le répertoire courant 
 
 # Q1  Imprimez le répertoire courant
 print(f"Q1{'_'*60}")
+repertoire_courant=os.getcwd()
+print(repertoire_courant)
 
 
 # Q2   Imprimez la variable d'environnement USERPROFILE (utilisez la méthode .get() de l'objet os.environ)
 print(f"Q2{'_'*60}")
+info_user=os.environ
+username:str=info_user.get("USERPROFILE")
+print(username)
 
 # Q3 Déplacez-vous sur le répertoire 'Documents' et imprimez le répertoire courant
 #       Attention : n'utilisez pas un chemin relatif.
 print(f"Q3{'_'*60}")
+repertoire=username +'\\Documents'
+os.chdir(repertoire)
+repertoire_changé=os.getcwd()
+print(repertoire_changé)
 
     # NOTE : On ne peut pas écrire c:\\Users\\..... parce que ca ne fonctionnerait pas sur votre ordinateur à la maison
 
 # Q4   Imprimez la liste des répertoires et des fichiers qu'il y a dans 'Document'
 print(f"Q4{'_'*60}")
-
+liste=os.listdir(repertoire)
+print(liste)
 
 # Q5   Créez un répertoire OS-ExercQ5
 #       Réimprimez les répertoires et les fichiers dans 'Document'
 print(f"Q5{'_'*60}")
-
+os.makedirs(f"{repertoire}\\OS-ExercQ5")
+liste=os.listdir(repertoire)
+print(liste)
 
 # Q6   Créez les répertoires OS-ExercQ6/Subdir1 avec une seule instruction
 #       Réimprimez les répertoires et les fichiers dans votre 'Document'
 print(f"Q6{'_'*60}")
+os.makedirs(f"{repertoire}\\OS-ExercQ6\\Subdir1")
+liste=os.listdir(repertoire)
+print(liste)
 
 
 
 #Q7   Renommez le répertoire Subdir1 pour qu'il s'appelle Sous_repertoire
 print(f"Q6{'_'*60}")
-
+os.rename(f"{repertoire}\\OS-ExercQ6\\Subdir1",f"{repertoire}\\OS-ExercQ6\\Sous_repertoire")
 # Q8   suppression du répertoire OS-ExercQ6 et de son contenu
+os.rmdir(f"{repertoire}\\OS-ExercQ6\\Sous_repertoire")
+os.rmdir(f"{repertoire}\\OS-ExercQ6") 
 #       Réimprimez les répertoires et les fichiers dans votre 'Document'
 print(f"Q6{'_'*60}")
+liste=os.listdir(repertoire)
+print(liste)
 
 
 
--- Ex2_dict.py
+++ Ex2_dict.py
@@ -3,20 +3,47 @@ def obtenir_liste_noms(p_data:list[dict]) -> list[str] :
     """Retourne une liste des noms complets .
     Chaque indexe contient le prénom et nom du client"""
     # créée une liste vide de str
+    liste_nomcomplets:list[str]=[]
     # pour chaque valeur dans p_data
+    for valeur in p_data:
+        nom_client=valeur["nom"]
+        prenom_client=valeur["prenom"]
+        nom_complet_client=f"{prenom_client} {nom_client}"
+        liste_nomcomplets.append(nom_complet_client)
+    return liste_nomcomplets
     #       créée 1 str contenant le nom complète du client NOTE : Il est possible que VSCode ne soit pas capable de suggéré les clefs du dict.
     #       ajouté le nouveau nom à la liste de noms
     # retournée la liste de noms
 
 # Q2 : Créez la fonction "obtenir_solde_moyen", qui doit retourner un float.
+def obtenir_solde_moyen(p_data:list[dict]) -> float:
             # On peut savoir le nombre de client avec la fonction len()
+    nb_clients:int=len(p_data)
+    solde_total:int=0
+    solde_moyen:float=0
             # Si je veux avoir le solde moyen de tous mes clients, je vais devoir en premier calculer le solde total
+    for personne in p_data:
+        solde_total+=personne["solde"]
             # Et après, diviser le solde total par le nombre de clients
+        solde_moyen=solde_total/nb_clients
+    return solde_moyen
 
 
 
 # Q3 : Créez la fonction "obtenir_stats".
 #   Cette fonction doit retourner 3 valeurs, le total des soldes, la moyenne des soldes, et le nombre de clients
+def obtenir_stats(p_data:list[dict]) -> float:
+    nb_clients:int=len(p_data)
+    solde_total:int=0
+    solde_moyen:float=0
+    for personne in p_data:
+        solde_total+=personne["solde"]
+        solde_moyen=solde_total/nb_clients
+    return nb_clients,solde_moyen,solde_total
+
+
+
+
 
 
 
@@ -40,7 +67,8 @@ if __name__ == "__main__" :
     # Complètez la fonction "obtenir_liste_employe()"
     # Apellez la fonction, capturez le résultat et l'afficher
     #     Résultat attendu dans le terminal : "Voici la liste des clients: [ Hélène Boucher, Thérèse Tessier, Benjamin Savard, Jean Tremblay, Hugues Pelletier ]"
-
+    liste_nomcomplets=obtenir_liste_noms(donnees_json)
+    print(f"Voici la liste des clients: {liste_nomcomplets}")
 
 
     print(f"Q2{80*'_'}")
@@ -48,7 +76,8 @@ if __name__ == "__main__" :
     # La fonction "obtenir_solde_moyen" n'extiste pas ! C'est à vous de la crée.
     # Vous devez l'appeler et capturer son retour puis imprimer le solde moyen.
     #       Résultat attendu dans le terminal:  "Le solde moyen de mes clients est: 3773.6 $"
-
+    solde_moyen=obtenir_solde_moyen(donnees_json)
+    print(f"Le solde moyen de mes clients est: {solde_moyen}")
 
     print(f"Q3{80*'_'}")
     # Q2 : La fonction précédente était un peu simpliste.
@@ -59,3 +88,5 @@ if __name__ == "__main__" :
     #   le nombre de clients
     # Vous devez l'appeler et capturer son retour puis l'imprimer
     #       Résultat attendu dans le terminal:  "Total : 18868 $, moy : 3773.6 $, nombre de clients : 5"
+solde_total=obtenir_stats(donnees_json)
+print(f"Total: {solde_total[2]}, moyenne: {solde_total[1]}, nombre de clients: {solde_total[0]}")
